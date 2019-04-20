cb706a4f02a75f260162dc1441bbe40ba987db95
jvbsl <jvsbl@gmx.de>
2019-04-20 22:50:11 +0200

    set llvm environment variable

--- build.ps1
+++ build.ps1
@@ -6,12 +6,13 @@ New-Item -ItemType Directory -Force -Path build\x86_64
 
 Set-Location -Path mesa
 
+$env:LLVM = "C:\Libraries\llvm-4.0.0"
 
-scons machine=x86_64 llvm=yes swr=yes libgl-gdi
+scons machine=x86_64 llvm=yes libgl-gdi
 
 Copy-Item "build\windows-x86_64-debug\gallium\targets\libgl-gdi\*" -Destination ..\build\x86_64 -Recurse
 
-scons machine=x86 llvm=yes swr=yes libgl-gdi
+scons machine=x86 llvm=yes libgl-gdi
 
 Copy-Item "build\windows-x86-debug\gallium\targets\libgl-gdi\*" -Destination ..\build\x86 -Recurse
 
