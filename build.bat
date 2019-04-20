42d6034e08297566ff68f351fc9e3f13fb357165
jvbsl <jvsbl@gmx.de>
2019-04-20 18:24:28 +0200

    deactivate rdp

--- build.bat
+++ build.bat
@@ -5,12 +5,13 @@ mkdir ..\build\ 2> NUL
 mkdir ..\build\x86 2> NUL
 mkdir ..\build\x86_64 2> NUL
 
+scons machine=x86_64 libgl-gdi
+
+cp build\windows-x86_64-debug\gallium\targets\libgl-gdi\ ..\build\x86_64 2> NUL
+
 scons machine=x86 libgl-gdi
 
 cp build\windows-x86-debug\gallium\targets\libgl-gdi\ ..\build\x86 2> NUL
 
-scons machine=x86_64 libgl-gdi
-
-cp build\windows-x86_64-debug\gallium\targets\libgl-gdi\ ..\build\x86_64 2> NUL
 
 7z a ..\opengl_win.zip ..\build\
