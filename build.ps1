b5a1a72c2fe4bd622b148c0de413a5dd6af3020a
jvbsl <jvsbl@gmx.de>
2019-04-20 22:21:54 +0200

    compile with software renderer

--- build.ps1
+++ build.ps1
@@ -7,11 +7,11 @@ New-Item -ItemType Directory -Force -Path build\x86_64
 Set-Location -Path mesa
 
 
-scons machine=x86_64 libgl-gdi
+scons machine=x86_64 swr=yes libgl-gdi
 
 Copy-Item "build\windows-x86_64-debug\gallium\targets\libgl-gdi\*" -Destination ..\build\x86_64 -Recurse
 
-scons machine=x86 libgl-gdi
+scons machine=x86 swr=yes libgl-gdi
 
 Copy-Item "build\windows-x86-debug\gallium\targets\libgl-gdi\*" -Destination ..\build\x86 -Recurse
 
