d9fada638eb4cb5ab7c25781d589ce4fe747ad92
jvbsl <jvsbl@gmx.de>
2019-04-20 16:26:37 +0200

    create artifacts(?)

--- build.bat
+++ build.bat
@@ -1,4 +1,16 @@
 git clone https://gitlab.freedesktop.org/mesa/mesa.git
 cd mesa
+
+mkdir ..\build\ 2> NUL
+mkdir ..\build\x86 2> NUL
+mkdir ..\build\x86_64 2> NUL
+
 scons machine=x86 libgl-gdi
+
+cp build\windows-x86-debug\gallium\targets\libgl-gdi\ ..\build\x86 2> NUL
+
 scons machine=x86_64 libgl-gdi
+
+cp build\windows-x86_64-debug\gallium\targets\libgl-gdi\ ..\build\x86_64 2> NUL
+
+7z a ..\opengl_win.zip ..\build\
