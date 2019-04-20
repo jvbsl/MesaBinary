b9ee250f98dc876a598fd9365abaa154db15da27
jvbsl <jvsbl@gmx.de>
2019-04-20 18:35:51 +0200

    use powershell to build

---
+++ build.ps1
@@ -0,0 +1,19 @@
+git clone https://gitlab.freedesktop.org/mesa/mesa.git
+
+New-Item -ItemType Directory -Force -Path build
+New-Item -ItemType Directory -Force -Path build\x86
+New-Item -ItemType Directory -Force -Path build\x86_64
+
+Set-Location -Path mesa
+
+
+scons machine=x86_64 libgl-gdi
+
+Copy-Item build\windows-x86_64-debug\gallium\targets\libgl-gdi\ -Destination ..\build\x86_64 -Recurse
+
+scons machine=x86 libgl-gdi
+
+Copy-Item build\windows-x86-debug\gallium\targets\libgl-gdi\ -Destination ..\build\x86 -Recurse
+
+Set-Location -Path ..
+7z a opengl_win.zip build\
