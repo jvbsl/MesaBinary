d009ff4b086fd03d0246b2ec83b0adfcf11109aa
jvbsl <jvsbl@gmx.de>
2021-11-07 13:44:17 +0100

    Updated to meson build setup

---
+++ build32.ps1
@@ -0,0 +1,16 @@
+$BISONFLEX=Join-Path -Path (Get-Location) -ChildPath "winflexbison"
+$ENV.Path += ";$BISONFLEX"
+
+Set-Location -Path mesa
+
+git clean -xdf
+
+meson configure
+
+meson builddir/ --buildtype=release --backend=vs
+
+msbuild builddir/mesa.sln
+
+Copy-Item "builddir\src\gallium\targets\libgl-gdi\*" -Exclude "*@@*" -Destination ..\build\x86 -Recurse
+
+Set-Location -Path ..
